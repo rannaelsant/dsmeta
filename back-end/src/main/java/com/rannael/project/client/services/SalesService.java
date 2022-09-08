49662bb0ec8b9ca92239018103fc9b0960f6c60d
Rannael <74615002+rannaelsant@users.noreply.github.com>
2022-09-08 17:02:30 -0300

    Date Select

--- back-end/src/main/java/com/rannael/project/client/services/SalesService.java
+++ back-end/src/main/java/com/rannael/project/client/services/SalesService.java
@@ -1,20 +1,30 @@
 package com.rannael.project.client.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.rannael.project.client.entities.Sale;
 import com.rannael.project.client.repositories.SalesRepository;
 
-@Service
+@Servicei
 public class SalesService {
 	
 	@Autowired
 	private SalesRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 }
